a9c3480c01e80ab7e35670a0a0a674e27b8b4364
ek7 <kentikzaraki@gmail.com>
2016-09-17 15:17:50 +0300

    authorization ver 0.1

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index',
-      { title: 'Welcome | Tweechat',
-        language:'Русский'
-      });
+    if(req.session.name==null){
+      res.render('index',
+          { title: 'Welcome | Tweechat',
+            language:'Русский'
+          });
+    }
+    else{
+       res.redirect('/profile');
+    }
 });
 
 module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user').User;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+
+router.get('/', function(req, res) {
   res.render('profile',{
-    title:"Tweechat",
-    language:"Русский"
+    title: "Tweechat | Profile",
+    name:req.session.name,
+    surname:req.session.surname,
+    language:req.session.language
+  })
+});
+
+
+router.post('/login',function (req,res) {
+  User.findOne({email:req.body.email},function (err,user) {
+    if(err) res.send('Error in db');
+    if(user.email!=null){
+      if(user.password==req.body.password){
+        req.session.name=user.name;
+        req.session.surname=user.surname;
+        req.session.language=user.language;
+        res.redirect('/profile');
+      }
+      else{
+        res.send("Incorrect password");
+      }
+    }
+    else {
+      res.send("Wrong users email");
+    }
   })
 });
 

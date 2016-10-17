efb03c0dce4df474d66da5043d97dc8fc39561d3
Vladimir Liashyk <vladimir.liashyk@gmail.com>
2016-10-17 20:32:02 +0300

    ver 0.23

--- app.js
+++ app.js
@@ -5,16 +5,27 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
-
-var routes = require('./routes/index');
-var register=require('./routes/register');
+var debug = require('debug')('tweechat:server');
+var http = require('http');
 
 var app = express();
+var routes = require('./routes/index');
+var register=require('./routes/register');
+var port = normalizePort(process.env.PORT || '3000');
+var io = require('socket.io')(http);
+app.set('port', port);
+
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('view cache', false);
 
+io.listen(server);
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -62,5 +73,68 @@ app.use(function(err, req, res, next) {
   });
 });
 
+io.on('connection',function (socket) {
+  console.log("new connection");
+  socket.on('disconnect',function () {
+    console.log("disconnected");
+  })
+});
+
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
 module.exports = app;
